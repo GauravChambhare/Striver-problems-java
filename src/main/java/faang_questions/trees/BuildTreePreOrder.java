dbce01ff80eddc76f027d16afb5879ddc8dd27d3
GauravChambhare <gauravchambhare236@gmail.com>
2025-03-30 14:59:22 +0530

    -

---
+++ src/main/java/faang_questions/trees/BuildTreePreOrder.java
@@ -0,0 +1,34 @@
+package faang_questions.trees;
+
+
+public class BuildTreePreOrder {
+
+    // first create a node class
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        // this is the constructor for the Node class
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+    static class BinaryTree{
+        static Node BuildTree(int arr[]){
+
+            
+
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        // declare a int arr containing preorder traversal
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    }
+    
+}
