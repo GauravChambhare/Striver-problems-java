5bcd992be2dee8822042ae19b7e2c728dfc0c682
GauravChambhare <gauravchambhare236@gmail.com>
2025-03-31 20:10:01 +0530

    -

---
+++ src/main/java/a2z/step13/lec1/PreorderTraversalOfBT.java
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+144. Binary Tree Preorder Traversal
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+*/
+
+package a2z.step13.lec1;
+import java.util.*;
+
+public class PreorderTraversalOfBT {
+
+    public static List<Integer> preOrderTraversal(TreeNode root){
+        return preOrder(root, new ArrayList<Integer>() );
+    }
+    public static List<Integer> preOrder(TreeNode root, ArrayList<Integer> l){
+        if (root == null) {
+            return l;
+        }
+        l.add(root.data); // Visit the root
+        preOrder(root.left, l); // Traverse the left subtree
+        preOrder(root.right, l); // Traverse the right subtree
+        return l;
+    }
+
+
+
+    public static void main(String[] args) {
+        // {1,2,3,4,5,6,7} -- binary tree values
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Binary tree input values are: 1, 2, 3, 4, 5, 6, 7");
+        List<Integer> list = preOrderTraversal(root);
+        for(Integer r : list){
+            System.out.print(r + "->");
+        }
+        System.out.println("null");
+    }
+    
+}
